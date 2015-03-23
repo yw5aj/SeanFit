7cf6c406a8670ba631b1ec48ac2b71999d1736cd
Yuxiang Wang <yw5aj@virginia.edu>
2015-03-22 22:50:06 -0400

    Changed to two terms and weighted r2

--- fitprony.py
+++ fitprony.py
@@ -20,22 +20,20 @@ def displ2force(params, displ):
 
 
 def relax(params, time):
-    g1, tau1, g2, tau2, g3, tau3 = params
-    ginf = 1 - g1 - g2 - g3
+    g1, tau1, g2, tau2 = params
+    ginf = 1 - g1 - g2
     gr = g1 * np.exp(-time / tau1) +\
         g2 * np.exp(-time / tau2) +\
-        g3 * np.exp(-time / tau3) +\
         ginf
     return gr
 
 
 def creep(params, time, ginf):
-    c1, tau1, c2, tau2, c3, tau3 = params
-    cinf = 1 - c1 - c2 - c3
+    c1, tau1, c2, tau2 = params
+    cinf = 1 - c1 - c2
     cc = 1 + (1 -
               c1 * np.exp(-time / tau1) -
               c2 * np.exp(-time / tau2) -
-              c3 * np.exp(-time / tau3) -
               cinf
               ) * (1 / ginf - 1)
     return cc
@@ -52,9 +50,18 @@ def get_r2(params, time, displ, force):
     params_d2f = params[:2]
     params_rel = params[2:]
     force_predicted = predict_force(params_d2f, params_rel, time, displ)
-    sst = ((force - force.mean()) ** 2).sum()
-    sse = ((force - force_predicted) ** 2).sum()
-    r2 = 1 - sse / sst
+    maxidx = force.argmax()
+    force_ramp = force[:maxidx]
+    force_hold = force[maxidx:]
+    force_predicted_ramp = force_predicted[:maxidx]
+    force_predicted_hold = force_predicted[maxidx:]
+    sst_ramp = ((force_ramp - force_ramp.mean()) ** 2).sum()
+    sse_ramp = ((force_ramp - force_predicted_ramp) ** 2).sum()
+    sst_hold = ((force_hold - force_hold.mean()) ** 2).sum()
+    sse_hold = ((force_hold - force_predicted_hold) ** 2).sum()
+    r2_ramp = 1 - sse_ramp / sst_ramp
+    r2_hold = 1 - sse_hold / sst_hold
+    r2 = np.mean((r2_ramp, r2_hold))
     return r2
 
 
@@ -83,10 +90,9 @@ if __name__ == '__main__':
         return sign * avg_r2
     bounds = ((0, None), (0, None),
               (0, 1), (0, None),
-              (0, 1), (0, None),
               (0, 1), (0, None))
-    constraints = ({'type': 'ineq', 'fun': lambda x: 1 - x[2] - x[4] - x[6]})
-    x0 = (1e-2, 5, .3, .1, .3, 1, .3, 10)
+    constraints = ({'type': 'ineq', 'fun': lambda x: 1 - x[2] - x[4]})
+    x0 = (1e-2, 5, .3, .1, .3, 1)
     res = minimize(get_avg_r2, x0, args=(-1), method='SLSQP',
                    bounds=bounds, constraints=constraints)
     params = res.x
