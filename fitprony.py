94efcfcc9fd8881867ad88508341bc6854070ad7
Yuxiang Wang <yw5aj@virginia.edu>
2015-03-24 13:44:11 -0400

    Fit to individual traces

--- fitprony.py
+++ fitprony.py
@@ -12,6 +12,8 @@ import matplotlib.pyplot as plt
 
 
 DT = 1E-3
+ND2F = 2
+FT = .002  # contact threshold
 
 
 def displ2force(params, displ):
@@ -34,16 +36,6 @@ def relax(params, time):
     return gr
 
 
-def creep(params, time, cinf):
-    c1, tau1, c2, tau2, c3, tau3 = params
-    cc = 1 + (1 -
-              c1 * np.exp(-time / tau1) -
-              c2 * np.exp(-time / tau2) -
-              c3 * np.exp(-time / tau3)
-              ) * (cinf - 1)
-    return cc
-
-
 def predict_force(params_d2f, params_rel, time, displ):
     force_elastic = displ2force(params_d2f, displ)
     gr = relax(params_rel, time)
@@ -51,38 +43,14 @@ def predict_force(params_d2f, params_rel, time, displ):
     return force
 
 
-def predict_displ(params_d2f, params_crp, time, force, cinf):
-    displ_elastic = force2displ(params_d2f, force)
-    cc = creep(params_crp, time, cinf)
-    displ = np.convolve(cc, np.gradient(displ_elastic) / DT)[:time.size] * DT
-    return displ
-
-
-def get_force_sse(params, time, displ, force):
-    params_d2f = params[:2]
-    params_rel = params[2:]
+def get_force_sse(fitx, time, displ, force, x0):
+    params = fitx * x0
+    params_d2f = params[:ND2F]
+    params_rel = params[ND2F:]
     force_predicted = predict_force(params_d2f, params_rel, time, displ)
-    sse = ((force - force_predicted) ** 2).sum()
+    residual = force - force_predicted
+    sse = ((residual / force.mean()) ** 2).sum()
     return sse
-
-
-def get_force_r2(params, time, displ, force):
-    params_d2f = params[:2]
-    params_rel = params[2:]
-    force_predicted = predict_force(params_d2f, params_rel, time, displ)
-    maxidx = force.argmax()
-    force_ramp = force[:maxidx]
-    force_hold = force[maxidx:]
-    force_predicted_ramp = force_predicted[:maxidx]
-    force_predicted_hold = force_predicted[maxidx:]
-    sst_ramp = ((force_ramp - force_ramp.mean()) ** 2).sum()
-    sse_ramp = ((force_ramp - force_predicted_ramp) ** 2).sum()
-    sst_hold = ((force_hold - force_hold.mean()) ** 2).sum()
-    sse_hold = ((force_hold - force_predicted_hold) ** 2).sum()
-    r2_ramp = 1 - sse_ramp / sst_ramp
-    r2_hold = 1 - sse_hold / sst_hold
-    r2 = np.mean((r2_ramp, r2_hold))
-    return r2
 
 
 if __name__ == '__main__':
@@ -93,34 +61,37 @@ if __name__ == '__main__':
         time = pd.read_csv('./data/%dmN_time.txt' % stimulus).values.T[0]
         displ = pd.read_csv('./data/%dmN_disp.txt' % stimulus).values.T[0]
         force = pd.read_csv('./data/%dmN_force.txt' % stimulus).values.T[0]
-        contact_idx = (displ > 0).nonzero()[0][0]
+        contact_idx = (force > FT).nonzero()[0][0] - 100
         time_list.append(time[contact_idx:contact_idx + 5000]
                          - time[contact_idx])
-        displ_list.append(displ[contact_idx:contact_idx + 5000])
         force_list.append(force[contact_idx:contact_idx + 5000])
+        # Make displacement curve
+        displ_static = displ[contact_idx + 2500]
+        maxidx = force_list[-1].argmax()
+        displ_new = np.empty_like(force_list[-1])
+        displ_new[:maxidx] = np.linspace(0, displ_static, maxidx)
+        displ_new[maxidx:] = displ_static
+        displ_list.append(displ_new)
     # %% Start fitting the force trances
-
-    def get_avg_force_r2(params, sign=1.):
-        r2_list = []
-        for i, displ in enumerate(displ_list):
-            force = force_list[i]
-            time = time_list[i]
-            r2_list.append(get_force_r2(params, time, displ, force))
-        avg_r2 = np.mean(r2_list)
-        return sign * avg_r2
-    bounds = ((0, None), (0, None),
-              (0, 1), (1e-2, None),
-              (0, 1), (1e-2, None),
-              (0, 1), (1e-2, None))
-    constraints = ({'type': 'ineq', 'fun': lambda x: 1 - np.sum(x[2::2])})
-    x0 = (1e-2, 5, .3, .1, .3, 1, .3, 10)
-    res = minimize(get_avg_force_r2, x0, args=(-1), method='SLSQP',
-                   bounds=bounds, constraints=constraints)
-    params = res.x
-    avg_r2 = -res.fun
-    params_rel = params[2:]
-    params_d2f = params[:2]
-    ginf = 1 - np.sum(params_rel[::2])
+    params_list, params_d2f_list, params_rel_list = [], [], []
+    for i, stimulus in enumerate(stimuli_list):
+        force = force_list[i]
+        displ = displ_list[i]
+        time = time_list[i]
+        bounds = ((0, None), (0, None),
+                  (0, 1), (DT, None),
+                  (0, 1), (DT, None),
+                  (0, 1), (DT, None))
+        constraints = ({'type': 'ineq',
+                        'fun': lambda x: 1 - np.sum(x[ND2F::2])})
+        x0 = np.array([.01, 7, .3, .001, .3, .5, .3, 10])
+        res = minimize(get_force_sse, np.ones(8),
+                       args=(time, displ, force, x0),
+                       method='SLSQP', bounds=bounds, constraints=constraints)
+        params = res.x * x0
+        params_list.append(params)
+        params_rel_list.append(params[ND2F:])
+        params_d2f_list.append(params[:ND2F])
     # %% Find point by point
     resolution = 1e-2
     ramp_time = .5
@@ -129,6 +100,8 @@ if __name__ == '__main__':
     time = np.arange(500) * resolution
     fine_time = np.arange(5000) * DT
     for i, stimulus in enumerate(stimuli_list):
+        params_d2f = params_d2f_list[i]
+        params_rel = params_rel_list[i]
         force_cmd = np.empty_like(time)
         force_cmd[:maxidx] = np.linspace(0, stimulus * 1e-3, maxidx)
         force_cmd[maxidx:] = stimulus * 1e-3
@@ -145,19 +118,22 @@ if __name__ == '__main__':
                 sse = ((current_force - predicted_force) ** 2).sum()
                 return sse
             d0 = force2displ(params_d2f, stimulus * 1e-3)
-            d = minimize(get_current_sse, d0, method='L-BFGS-B').x
+            d = minimize(get_current_sse, d0, method='L-BFGS-B',
+                         bounds=((0, None), )).x
             displ[idx + 1] = d
         displ_cmd_list.append(np.interp(fine_time, time, displ))
         force_cmd_list.append(np.interp(fine_time, time, force_cmd))
         force_model_list.append(predict_force(params_d2f, params_rel,
                                               fine_time, displ_cmd_list[-1]))
     # %% Plot the fitting
-    fig, axs = plt.subplots(3, 3, figsize=(7, 7.5))
+    fig, axs = plt.subplots(3, 3, figsize=(7, 7))
     for i, displ in enumerate(displ_list):
         # First column, fitting
         force = force_list[i]
         time = time_list[i]
-        force_predicted = predict_force(params[:2], params[2:], time, displ)
+        params_d2f = params_d2f_list[i]
+        params_rel = params_rel_list[i]
+        force_predicted = predict_force(params_d2f, params_rel, time, displ)
         axs[i, 0].plot(time, force, '.', color='.5', label='Data')
         axs[i, 0].plot(time, force_predicted, '-k', label='Model')
         # Second column, commanded displ
